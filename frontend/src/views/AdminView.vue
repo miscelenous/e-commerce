954d3d19557d0493d78148f75c4edc518f787062
Awipratama <awipratama05122006@gmail.com>
2024-04-02 14:56:22 +0800

    Make frontend

---
+++ frontend/src/views/AdminView.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import "@/assets/style.css";
+</script>
+<template>
+  <p></p>
+</template>
+<style scoped></style>
---
+++ frontend/src/views/layout/UserLayout.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import Navigation from "@/components/UserNav.vue";
+</script>
+<template>
+  <RouterView />
+  <Navigation />
+</template>
